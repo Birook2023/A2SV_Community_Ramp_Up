b627a461a8d459b96319cba6a9196a5777b88f5f
Birook2023 <122909706+Birook2023@users.noreply.github.com>
2023-02-17 23:05:27 +0300

    Create Sort List.py

---
+++ Sort List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = []
+        curr = head
+        while curr:
+            nums.append(curr.val)
+            curr = curr.next
+            
+        nums.sort()                               
+        curr = head
+        for i in nums:
+            curr.val = i
+            curr = curr.next
+            
+        return head
+
+            
